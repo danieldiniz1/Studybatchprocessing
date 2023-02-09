9ba7761c025f2c50119a748b2c418bf612edbab3
Daniel Alexandre Diniz <daniel.alexandrediniz@emeal.nttdata.com>
2023-02-09 17:56:30 -0300

    first commit

---
+++ src/main/java/br/com/batch/studybatchprocessing/StudybatchprocessingApplication.java
@@ -0,0 +1,13 @@
+package br.com.batch.studybatchprocessing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudybatchprocessingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudybatchprocessingApplication.class, args);
+	}
+
+}
